1e2caee79d4eccf33bcfd50223fba957fb42f034
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-08-29 14:22:44 -0700

    Using inline jquery ui calendar for home show search selection

--- public/javascripts/map_home_show.js
+++ public/javascripts/map_home_show.js
@@ -5,6 +5,7 @@ var bounds = new google.maps.LatLngBounds();
 $(function() {
   set_heights()
   $(window).resize(set_heights);
+  handle_datepicker();
   if(center != undefined && trips.length > 0) {
     map = new google.maps.Map(document.getElementById("map_canvas"), {
       center: new google.maps.LatLng(center[0], center[1]),
@@ -12,9 +13,6 @@ $(function() {
     });
  
     for(var i = 0; i<trips.length; i++) { drawRoute(trips[i]) }
-    //$(origin_trips).each(drawRouteFromPlaces);
-    //$(destination_trips).each(drawRouteFromPlaces);
-    //$(perfect_matches).each(drawRouteFromPlaces);
     map.fitBounds(bounds);
   }
 });
@@ -47,4 +45,39 @@ function set_heights() {
   base = Math.max(base, parseInt($("#content").css("min-height")))
   $("#map_canvas").css("height", base+"px");
   $("#map_canvas").css("width", window.innerWidth+"px");
+}
+
+function unavailable(_date) {
+  var unavailableDates = ["9-9-2011","14-9-2011","15-9-2011"];
+  dmy = _date.getDate() + "-" + (_date.getMonth()+1) + "-" + _date.getFullYear();
+  if ($.inArray(dmy, unavailableDates) == -1) {
+    return [true, ""];
+  } else {
+    return [false,"","Unavailable"];
+  }
+}
+
+function handle_datepicker() {
+  var _dates = [];
+  $("#start_date").datepicker({
+    altField: "#alternate",
+    altFormat: "yy-mm-d",
+    defaultDate: +6,
+    minDate: new Date(),
+    beforeShowDay: function(_date){
+      if($.inArray(_date.getTime(),_dates)>=0)
+        return [true,"highlighted-week","Week Range"];
+      return[true,"",""];
+    },
+    onSelect: function(_selectedDate){
+      var _date = new Date(_selectedDate);
+      _dates =[];
+      var tolerance = flexibility[$("#start_flexibility :selected").val()];
+      for (i = -tolerance[0]; i <= tolerance[1]; i++) {
+        var tempDate=new Date(_date.getTime());
+        tempDate.setDate(tempDate.getDate()+i);
+        _dates.push(tempDate.getTime());
+      }
+    }
+  })
 }
